e33513a9282b5408f7fe0d7413fd1cb9a7204653
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2023-05-18 19:43:38 +0300

    [DOP-4594] Increase max memory limit to 512Mb

--- hdfs/conf/hadoop/hadoop-env.sh
+++ hdfs/conf/hadoop/hadoop-env.sh
@@ -2,4 +2,4 @@ export JAVA_HOME=/opt/java/openjdk
 export HDFS_DATANODE_USER=root
 export HDFS_NAMENODE_USER=root
 export HDFS_SECONDARYNAMENODE_USER=root
-export HADOOP_HEAPSIZE=512s
+export HADOOP_HEAPSIZE=512
